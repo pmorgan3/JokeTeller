34b9b74263653bb5202aa33d3952e49d0a0a1226
Paul Morgan III <pmorgan3@gmail.com>
2020-05-18 14:41:25 -0500

    cleaned code

--- lib/joke_teller.ex
+++ lib/joke_teller.ex
@@ -9,7 +9,10 @@ defmodule JokeTeller do
     url = "https://sv443.net/jokeapi/v2/joke/Programming?type=twopart"
     case HTTPoison.get(url, ["Accept": "Application/json; Charset=utf-8"], [ssl: [{:versions, [:'tlsv1.2']}]]) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {_status, list} = body |> HTTPoison.process_response_body |> JSON.decode
+        {_status, list} =
+          body
+          |> HTTPoison.process_response_body
+          |> JSON.decode
         IO.puts list["setup"]
         IO.puts list["delivery"]
       {:ok, %HTTPoison.Response{status_code: 404}} ->
